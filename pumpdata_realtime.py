b9105fa1ba98e1acc1f22395bcfa720ab7f5ef24
Tushar Sharma <tsharma@HW15645.local>
2018-10-03 15:15:31 +0530

    sample+realtime+parameterized

--- pumpdata_realtime.py
+++ pumpdata_realtime.py
@@ -6,6 +6,7 @@ import threading
 import time
 import itertools
 import commands
+import requests
 
 class record:
 	def __init__(self):
@@ -39,27 +40,56 @@ class record:
 		self.namespaces=open("namespace.txt","r+").readlines()
 		self.pages=open("page.txt","r+").readlines()
 		self.comments=open("comment.txt","r+").readlines()
-		self.index=0
-		self.kafkaString="echo '{}' | /usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list c01s02.hadoop.local:6667 --topic wikipedia1"
+		with open("params.json") as params:
+			self.params=json.load(params)
+		self.kafkaString="echo '{}' | /usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --broker-list c01s02.hadoop.local:6667 --topic "+self.params["wrap"]["topicName"]
 
-	def runCommand(self,command):
+
+	def runSetup(self,topicName):
+		self.postSpec(self.params["wrap"]["supervisor_address"],self.params["wrap"]["ingestionSpec"])
+		self.runCommand("/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper localhost:2181 --create --topic {} --replication-factor 1 --partitions 1".format(topicName),True)
+
+	def postSpec(self,supervisor_address,ingestionSpec):
 		try:
+			resp=requests.post(supervisor_address+"/druid/indexer/v1/supervisor",headers={"Content-Type":"application/json"},data=json.dumps(ingestionSpec))
+			print resp.content
+			if not(resp.status_code==200):
+				raise Exception
+		except Exception as e:
+			print e
+			print "Ingestion Task not submitted to supervisor. Will stop here.."
+			sys.exit(1)
+
+	def runCommand(self,command,printOutput=False):
+		print command
+		try:	
 			s,o=commands.getstatusoutput(command)
-		except Exception:
+			if printOutput:
+				print s
+				print o
+		except Exception as e:
+			print e
 			pass
 
 	def getRecord(self):
 		while True:
-			deltasecs=random.choice(range(4))
-			time.sleep(deltasecs)
+			deltamillisecs=random.choice(range(self.params["wrap"]["max_wait_per_clicker"]))
+			time.sleep(deltamillisecs/1000.0)
 			try:
+				self.runCommand(self.kafkaString.format(json.dumps(self.alterTimeStamp(json.loads(self.basedata.next())))))
+			except StopIteration:
 				self.runCommand(self.kafkaString.format(json.dumps(self.buildRecord())))
 			except Exception:
 				pass
 		
+	def alterTimeStamp(self,sample_json):
+		sample_json["time"]=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+"Z"
+		print sample_json["time"]
+		return sample_json
+
 	def buildRecord(self):
 		rec=dict(self.basejson)
-		rec["time"]=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.200Z")
+		rec["time"]=datetime.datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")[:-3]+"Z"
 		rec["channel"]=random.choice(self.channels).strip(" \n")
 		rec["namespace"]=random.choice(self.namespaces).strip(" \n")
 		rec["user"]=random.choice(self.users).strip(" \n")
@@ -81,7 +111,9 @@ class record:
 		for t in gthreads:
 			t.join()
 
-		
-
 p=record()
-p.generateData(10)
+
+if p.params["wrap"]["runSetup"].lower()=="true":
+	p.runSetup(p.params["wrap"]["topicName"])
+
+p.generateData(p.params["wrap"]["clickers"])
